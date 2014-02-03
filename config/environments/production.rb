fc87841c0c7e11acc288dce6406434c2bdae96fd
Josh Rojas <rojos@seas.upenn.edu>
2014-02-03 18:31:57 -0500

    homework 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,9 @@ TestApp::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.initialize_on_precompile = false
+
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
